7eb466f661bcc3b0a5426008553bef9f89bad7f7
tylerGarland <32379919+tylerGarland@users.noreply.github.com>
2017-09-28 22:15:50 -0500

    Add files via upload

---
+++ SpherocylinderApp.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SpherocylinderApp{
+   Scanner userInput = new Scanner(System.in);
+   double radius;
+   public void main(String[] args){
+      System.out.println("Enter label, radius, and cylinder height for a "
+         + "spherocylinder.");
+      System.out.println("\tlabel: ");
+      radius = userInput.nextLine;
+      if( radius < 0 )
+      
+      {
+         System.out.print("One with a bad radius");
+      }
+      
+      
+   }
+}
