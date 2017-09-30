42d5f38997c6a7ba0c57168dcffd58880b4fe197
tylerGarland <32379919+tylerGarland@users.noreply.github.com>
2017-09-29 23:13:08 -0500

    Add files via upload

--- Spherocylinder.java
+++ Spherocylinder.java
@@ -1,35 +1,70 @@
 import java.text.DecimalFormat;
+/** To set up a constructor.
+*To set up a
+ */
 public class Spherocylinder {
 /**
-*Program to create a spherocylinder 
+*Program to create a spherocylinder constructor.
 */
    private String label = "";
    private double radius = 0;
    private double cylinderHeight = 0;
-   
-   public Spherocylinder(String label, double radius,
-   double ch){
-      setLabel(label);
-      setRadius(radius);
+   /**
+   *Calls on label, radius, ch.
+   @param labelin label of the spherocylinder.
+   @param radiusin radius for spherocylinder.
+   @param ch cylinderheight for spherocylinder.
+   */
+   public Spherocylinder(String labelin, double radiusin,
+      double ch) {
+      setLabel(labelin);
+      setRadius(radiusin);
       setCylinderHeight(ch);
    }
+   /**
+   *Gives value to label.
+   */
+   boolean setLabel(String labelIn) {
+      if (labelIn != labelIn.trim()) {
+         return false;
+      }
    
-   void setLabel(String labelIn) {
       label = labelIn;
+      
+      return true;
    }
-   void setRadius(double radiusIn) {
+   /**
+   *Makes it where radius has a value and if its less
+   *than 0 it returns false.
+   */
+   boolean setRadius(double radiusIn) {
+      if (radiusIn < 0) {
+         return false;
+      }
       radius = radiusIn;
+      return true;
    }
-   void setCylinderHeight(double chIn) {
+   /**
+   *Makes it where ch has a value and if it less than 0
+   *then it returns false.
+   */
+   boolean setCylinderHeight(double chIn) {
+      if (chIn < 0) {
+         return false;
+      }
       cylinderHeight = chIn;
+      return true;
    }
+   /**
+   *Gives label a return value.
+   */
    String getLabel() {
       return label;
    }
    double getRadius() {
-      return radius;}
+      return radius; }
       
-   double getcylinderHeight() {
+   double getCylinderHeight() {
       return cylinderHeight;
     
    }
@@ -43,9 +78,14 @@ public class Spherocylinder {
    double volume() {
       return Math.PI * radius * radius * (4.0 / 3.0 * radius + cylinderHeight);
    }
+   /**
+   *Setups the string to be used in the app.
+   *@return string that is setup.
+   */
    public String toString() {
       DecimalFormat fmt = new DecimalFormat("#,##0.0##");
-      String string = "Spherocylinder \"" + getLabel() + "\" with radius " + fmt.format(radius)
+      String string = "Spherocylinder \"" + getLabel() + "\" with radius " 
+         + fmt.format(radius)
          + " and cylinder height " + fmt.format(cylinderHeight) + " has:\n\t"
          + "circumference = " + fmt.format(circumference()) + " units\n"
          + "\tsurface area = " + fmt.format(surfaceArea()) + " square units\n"
--- SpherocylinderApp.java
+++ SpherocylinderApp.java
@@ -1,18 +1,39 @@
 import java.util.Scanner;
-public class SpherocylinderApp{
-   Scanner userInput = new Scanner(System.in);
-   double radius;
-   public void main(String[] args){
+/**
+*Program to use the construcor we made for spherocylinder.
+*@tylerGarland
+*@version: 9/28/2017
+*/
+
+public class SpherocylinderApp {
+   /**
+   *@param args not used.
+   */
+   public static void main(String[] args) {
+      Scanner userInput = new Scanner(System.in);
+      
       System.out.println("Enter label, radius, and cylinder height for a "
-         + "spherocylinder.");
-      System.out.println("\tlabel: ");
-      radius = userInput.nextLine;
-      if( radius < 0 )
-      
-      {
-         System.out.print("One with a bad radius");
+            + "spherocylinder.");
+      System.out.print("\tlabel: ");
+      String label = userInput.nextLine().trim();
+      System.out.print("\tradius: ");
+      double radius = userInput.nextDouble();
+      if (radius < 0) {
+         System.out.println("Error: radius must be non-negative.");
+         return;
       }
+      System.out.print("\tcylinder height: ");
+      double cylinderheight = userInput.nextDouble();
+      if (cylinderheight < 0) {
+         System.out.println("Error: cylinder height must be non-negative.");
+         return;
+      }
+      Spherocylinder sc = new Spherocylinder(label, radius, cylinderheight);
+      System.out.println(sc.toString());
       
+   
+     
       
    }
+ 
 }
